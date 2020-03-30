b1abfa64f61a9455b905e889fff27c7b44a8b594
Katarzyna Sliwicka-Iwacz <sliwickakatarzyna@gmail.com>
2020-03-30 17:19:47 +0200

    added not found div in filter

--- script/background.js
+++ script/background.js
@@ -1,27 +1,27 @@
 let button = document.querySelector('#background_btn');
 
-const white_items = document.getElementsByClassName('white_background')
-const violet_items = document.getElementsByClassName('violet_background')
+const whiteItems = document.getElementsByClassName('white_background')
+const violetItems = document.getElementsByClassName('violet_background')
 
 function changeBackground(){
     let currentClass = button.className;
     if (currentClass === 'violet_button') {
-        for (let i = 0; i<white_items.length; i++) {
-            white_items[i].style.backgroundColor = '#dcb4d5';
-            white_items[i].style.borderColor = '#dcb4d5';
+        for (let i = 0; i<whiteItems.length; i++) {
+            whiteItems[i].style.backgroundColor = '#dcb4d5';
+            whiteItems[i].style.borderColor = '#dcb4d5';
         }
-        for (let i = 0; i<violet_items.length; i++) {
-            violet_items[i].style.backgroundColor = '#fff';
+        for (let i = 0; i<violetItems.length; i++) {
+            violetItems[i].style.backgroundColor = '#fff';
         }
         button.classList.remove('violet_button');
         button.classList.add('white_button')
     } else if (currentClass === 'white_button') {
-        for (let i = 0; i<white_items.length; i++) {
-            white_items[i].style.backgroundColor = '#fff';
-            white_items[i].style.borderColor = '#fff';
+        for (let i = 0; i<whiteItems.length; i++) {
+            whiteItems[i].style.backgroundColor = '#fff';
+            whiteItems[i].style.borderColor = '#fff';
         }
-        for (let i = 0; i<violet_items.length; i++) {
-            violet_items[i].style.backgroundColor = '#dcb4d5';
+        for (let i = 0; i<violetItems.length; i++) {
+            violetItems[i].style.backgroundColor = '#dcb4d5';
         }
         button.classList.remove('white_button');
         button.classList.add('violet_button')
--- script/filter.js
+++ script/filter.js
@@ -1,16 +1,27 @@
 let search = document.querySelector('#search_input')
 let calculatorItems = document.querySelectorAll('#calculators_container .flex_item')
+let notFound = document.querySelector('#not_found')
 
 search.addEventListener('keyup', (e) => {
     
     const filter = e.target.value.toLowerCase().trim();
+    let counter = 0
 
     calculatorItems.forEach((item) => {
-        let itemHeader = item.querySelector('h2')
+        let itemHeader = item.querySelector('h2');
+
         if (itemHeader.textContent.toLowerCase().includes(filter)){
-            item.style.display = 'block'
+            item.style.display = 'block',
+            counter++
         } else {
             item.style.display = 'none'
         }
     })
+
+    if (counter === 0) {
+        notFound.style.display = 'block'
+    } else {
+        notFound.style.display = 'none'
+    }
+
 })
