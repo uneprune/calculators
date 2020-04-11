6bff2a5a41f15c31ac8b6aba6a38f508e4669f8c
Katarzyna Sliwicka-Iwacz <sliwickakatarzyna@gmail.com>
2020-04-11 17:58:46 +0200

    background.script: transition and forEach

--- scripts/background.js
+++ scripts/background.js
@@ -1,27 +1,29 @@
 let button = document.querySelector('#background_btn');
 
-const whiteItems = document.getElementsByClassName('white_background')
-const violetItems = document.getElementsByClassName('violet_background')
+const whiteItems = Array.from(document.getElementsByClassName('white_background'));
+const violetItems = Array.from(document.getElementsByClassName('violet_background'));
+const violet = '#dcb4d5';
+const white = '#fff';
 
 function changeBackground(){
     let currentClass = button.className;
     if (currentClass === 'violet_button') {
-        for (let i = 0; i<whiteItems.length; i++) {
-            whiteItems[i].style.backgroundColor = '#dcb4d5';
-            whiteItems[i].style.borderColor = '#dcb4d5';
-        }
-        for (let i = 0; i<violetItems.length; i++) {
-            violetItems[i].style.backgroundColor = '#fff';
-        }
+        whiteItems.forEach(item => {
+            item.style.backgroundColor = violet;
+            item.style.borderColor = violet;
+        });
+        violetItems.forEach(item => {
+            item.style.backgroundColor = white
+        });
         button.classList.remove('violet_button');
         button.classList.add('white_button')
     } else if (currentClass === 'white_button') {
         for (let i = 0; i<whiteItems.length; i++) {
-            whiteItems[i].style.backgroundColor = '#fff';
-            whiteItems[i].style.borderColor = '#fff';
+            whiteItems[i].style.backgroundColor = white;
+            whiteItems[i].style.borderColor = white;
         }
         for (let i = 0; i<violetItems.length; i++) {
-            violetItems[i].style.backgroundColor = '#dcb4d5';
+            violetItems[i].style.backgroundColor = violet;
         }
         button.classList.remove('white_button');
         button.classList.add('violet_button')
