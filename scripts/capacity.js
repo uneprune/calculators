4fae9e4d20f8cd5f97b7a1905bfc5bec93f4b958
Katarzyna Sliwicka-Iwacz <sliwickakatarzyna@gmail.com>
2020-04-11 11:11:48 +0200

    rename scripts folder

---
+++ scripts/capacity.js
@@ -0,0 +1,31 @@
+function capacityCalc(){
+    let inputValue = parseFloat(document.querySelector('#capacity_value').value);
+    let selectBox = document.querySelector('#capacity_select_box');
+    let selectedOption = selectBox.options[selectBox.selectedIndex].text;
+    let literValue = document.querySelector('#liter_result')
+    let meterValue = document.querySelector('#cubic_meter_result')
+    let footValue = document.querySelector('#cubic_foot_result')
+
+    if (isNaN(inputValue) === false){
+        document.querySelector('#capacity_table').style.display = 'table';
+        document.querySelector('#capacity_nan').style.display = 'none';
+        document.querySelector('#capacity_nan').style.color = '#1d1d1b';
+        if (selectedOption === 'liter') {
+            literValue.innerHTML = `${(inputValue).toFixed(4)} l`;
+            meterValue.innerHTML = `${(inputValue*0.0010).toFixed(4)} m3`;
+            footValue.innerHTML = `${(inputValue*0.0353).toFixed(4)} ft3;`;
+        } else if (selectedOption === 'cubic meter') {
+            literValue.innerHTML = `${(inputValue*1000).toFixed(4)} l`;
+            meterValue.innerHTML = `${(inputValue).toFixed(4)} m3`;
+            footValue.innerHTML = `${(inputValue*35.3147).toFixed(4)} ft3;`
+        } else if (selectedOption === 'cubic foot') {
+            literValue.innerHTML = `${(inputValue*28.3168).toFixed(4)} l`;
+            meterValue.innerHTML = `${(inputValue*0.0283).toFixed(4)} m3`;
+            footValue.innerHTML = `${(inputValue).toFixed(4)} ft3;`
+        }
+    } else {
+        document.querySelector('#capacity_table').style.display = 'none';
+        document.querySelector('#capacity_nan').style.display = 'inline-block';
+        document.querySelector('#capacity_nan').style.color = 'red';
+    }
+}
---
+++ scripts/tip.js
@@ -0,0 +1,36 @@
+let bill = document.querySelector('#bill_amount')
+let tip = document.querySelector('#tip_percentage')
+
+let tipValueText = document.querySelector('#tip_value')
+let totalTipText = document.querySelector('#total_tip')
+let tipResultText = document.querySelector('#tip_result')
+
+function tipCalc(){
+    let billAmount = parseFloat(bill.value)
+    let tipPercentage = parseFloat(tip.value)
+
+    if (billAmount && tipPercentage) {
+        let tipValue = parseFloat((billAmount * tipPercentage/100).toFixed(2));
+        let totalTip = (billAmount + tipValue).toFixed(2);
+
+        tipValueText.innerHTML = `Tip amount is ${tipValue} PLN`
+        totalTipText.innerHTML = `Total amount is ${totalTip} PLN`
+        tipResultText.style.color = '#1d1d1b';
+    } else if ((billAmount === 0) || (tipPercentage === 0)) {
+        tipValueText.innerHTML = `Please, fill all fields`
+        totalTipText.innerHTML = `with numbers greater than 0`
+        tipResultText.style.color = 'red';
+    } else if (billAmount || tipPercentage) {
+        tipValueText.innerHTML = `Your result will be here`
+        totalTipText.innerHTML = `Please, fill all fields with numbers`
+        tipResultText.style.color = 'white';
+    } else {
+        tipValueText.innerHTML = `Your result will be here`
+        totalTipText.innerHTML = `Please, fill all fields with numbers`
+        tipResultText.style.color = '#1d1d1b';
+    }
+}
+
+
+bill.addEventListener('keyup', (e) => tipCalc());
+tip.addEventListener('keyup', (e) => tipCalc());
